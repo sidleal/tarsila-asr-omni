45d3386cafbd9c27b267c323386a7c2e56eb3427
Sidney Leal <sidleal@gmail.com>
2026-01-07 10:28:42 -0300

    code fine tune

--- run-tarsila-finetune.sh
+++ run-tarsila-finetune.sh
@@ -1,2 +1,3 @@
 export OUTPUT_DIR="/home/jovyan/omnilingual-asr/output"
+export OMP_NUM_THREADS=16
 python -m workflows.recipes.wav2vec2.asr $OUTPUT_DIR --config-file workflows/recipes/wav2vec2/asr/configs/llm-finetune-tarsila.yaml
